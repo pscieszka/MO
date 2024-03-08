a4155d826a118e187152a56ccf57526449b61b54
pscieszka <piotr.scieszka.epinote@gmail.com>
2024-03-08 20:48:45 +0100

    lab1 done?

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float epsilon_float = 1.0f;
+    int liczba_bitow = 0;
+
+    while (epsilon_float + 1.0f > 1.0f)
+    {
+		epsilon_float = epsilon_float / 2.0f;
+		liczba_bitow++;
+		
+	}
+	liczba_bitow--;
+	epsilon_float = epsilon_float * 2.0f;
+    cout << "Liczba bitow mantysy dla float: " << liczba_bitow << endl;
+	cout << "Epsilon maszynowy dla float: " << epsilon_float << endl;
+
+	double epsilon_double = 1.0;
+	liczba_bitow = 0;
+
+	while (epsilon_double + 1.0 > 1.0)
+	{
+		epsilon_double = epsilon_double / 2.0;
+		liczba_bitow++;
+	}
+	liczba_bitow--;
+	epsilon_double = epsilon_double * 2.0;
+	cout << "Liczba bitow mantysy dla double: " << liczba_bitow << endl;
+	cout << "Epsilon maszynowy dla double: " << epsilon_double << endl;
+
+	// zwiazek epsilonu maszynowego z precyzja arytmetyki to, ze im mniejszy epsilon maszynowy, tym wieksza precyzja arytmetyki
+	return 0;	
+
+}
+
+
